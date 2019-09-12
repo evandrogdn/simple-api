efbce451507044a38e4432b2599ab38106a8f931
evandro_gdn <evandrogardolinn@gmail.com>
2019-09-11 23:07:01 -0300

    [ADD] Adicionada URL para conexão com banco de dados via mongoose

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const cors = require('cors');
 
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+mongoose.connect(
+    "mongodb://localhost:27017/estoque",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 app.use((req, res, next) => {
     req.io = io;
