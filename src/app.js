f9b8cd73cf8a0e942ff574b288fdbc009e8715c3
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-09 19:17:55 -0300

    [FIX] #cors - Ajuste nos headers de resposta para adicionar o header Access-Control-Allow-Origin

--- src/app.js
+++ src/app.js
@@ -11,13 +11,15 @@ class App {
 
     middlewares() {
         this.server.use(express.json());
+        this.server.use((req, res, next) => {
+            res.append("Access-Control-Allow-Origin", ["*"]);
+            next();
+        });
     }
 
     routes() {
         this.server.use(require('./routes'));
     }
 }
-
-module.exports = new App().server;
 
 module.exports = new App().server;
