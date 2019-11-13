3b2992a4f32030a56665a3bb06bb9c35ec073202
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-13 20:09:00 -0300

    [ADD] #cors - Adicionados headers de cors para comunicação entre API e Client

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ class App {
     middlewares() {
         this.server.use(express.json());
         this.server.use((req, res, next) => {
-            res.append("Access-Control-Allow-Origin", ["*"]);
+            res.append("Access-Control-Allow-Origin", "*");
+            res.append("Content-Type", "Application/Json");
+            res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+            res.append("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
             next();
         });
     }
